06e7d99168cc40adfe5c8683ab91317a8df73205
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-11-09 21:59:02 -0300

    implementando BubbleSort

--- src/br/com/unip/aps/ordenacao/BubbleSort.java
+++ src/br/com/unip/aps/ordenacao/BubbleSort.java
@@ -5,7 +5,26 @@ public class BubbleSort {
     private BubbleSort() {}
 
     public static long sort(int[] array) {
-        return 0;
+
+        int aux;
+        boolean tradeStillHappening;
+
+        long startTime = System.currentTimeMillis();
+
+        do {
+            tradeStillHappening = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    aux = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = aux;
+                    tradeStillHappening = true;
+                }
+            }
+        } while (tradeStillHappening);
+
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
     }
 
 }
